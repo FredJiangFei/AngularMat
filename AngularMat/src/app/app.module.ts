19c8199c783ac8f4d4963e6846b8e8432fa0526e
FredJiang <jiang.fei@ffcg.se>
2018-12-30 07:02:02 +0800

    add components

--- AngularMat/src/app/app.module.ts
+++ AngularMat/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { BookComponent } from './book/book.component';
+import { AuthorComponent } from './author/author.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    BookComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
