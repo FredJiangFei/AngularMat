149d75c49056fc3f12e1fa9a157df20d242372e0
FredJiang <jiang.fei@ffcg.se>
2018-12-31 21:39:39 +0800

    Add icon

--- AngularMat/src/app/share/share.module.ts
+++ AngularMat/src/app/share/share.module.ts
@@ -1,36 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import {
-    MatIconModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatSidenavModule,
-    MatCardModule,
-    MatInputModule,
-    MatListModule,
-    MatSlideToggleModule,
-    MatGridListModule,
-    MatDialogModule,
-    MatAutocompleteModule,
-    MatMenuModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSelectModule,
-    MatPaginatorModule,
-    MatTabsModule,
-    MatBadgeModule,
-    MatChipsModule,
-    MatTableModule
-} from '@angular/material';
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { CdkTableModule } from '@angular/cdk/table';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
     exports: [
@@ -40,30 +17,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         RouterModule,
         HttpClientModule,
         BrowserAnimationsModule,
-
-        MatToolbarModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatButtonModule,
-        MatCardModule,
-        MatInputModule,
-        MatListModule,
-        MatSlideToggleModule,
-        MatGridListModule,
-        MatDialogModule,
-        MatAutocompleteModule,
-        MatMenuModule,
-        MatCheckboxModule,
-        MatRadioModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
-        MatSelectModule,
-        CdkTableModule,
-        MatTabsModule,
-        MatBadgeModule,
-        MatPaginatorModule,
-        MatChipsModule,
-        MatTableModule
+        MaterialModule
     ]
 })
 export class ShareModule { }
